5088dc86b99be4efb5448e606d88006ede336e9c
thinyadanarsu <thinyadanarsu1200@gmail.com>
2021-09-03 18:10:23 +0630

    initial commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    copyright&copy; <span>{{ year }}</span>
+  </footer>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
